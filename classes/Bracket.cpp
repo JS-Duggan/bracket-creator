77287fde3aee428bb54fa8249e984be367087bc8
Jack Duggan <jacksdggn@gmail.com>
2025-06-18 20:28:15 +0930

    feat: clean up game output

--- classes/Bracket.cpp
+++ classes/Bracket.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 #include "Bracket.h"
 
@@ -10,8 +11,11 @@ void Bracket::displayBracket() {
   for (int i = 0; i < roundsComplete + 1; i++) {
     std::cout << "Round " << i + 1 << ":" << std::endl;
     for (int j = 0; j < rounds[i].size(); j++) {
-      std::cout << "  " << "Game " << j + 1 << ":";
-      std::cout << "  " << rounds[i][j].Player1->getName() << " vs. " << rounds[i][j].Player2->getName() << std::endl;
+      std::cout << "  Game " << j + 1 << ":  ";
+      std::cout << std::left << std::setw(8) << rounds[i][j].Player1->getName() 
+                << " vs. " 
+                << std::left << std::setw(20) << rounds[i][j].Player2->getName() 
+                << std::endl;
     }
   }
 }
