0f3270d095444a27008643f1c4c60130e298d76c
Jack Duggan <jacksdggn@gmail.com>
2025-06-19 15:02:01 +0930

    feat: add winner highlighting

--- classes/Bracket.cpp
+++ classes/Bracket.cpp
@@ -3,6 +3,10 @@
 
 #include "Bracket.h"
 
+#define BASE  "\033[0m"
+#define GREEN "\033[32m"
+#define RED   "\033[31m"
+
 Bracket::Bracket() {}; 
 
 Bracket::Bracket(std::vector<Player> players) : players(players){};
@@ -11,11 +15,18 @@ void Bracket::displayBracket() {
   for (int i = 0; i < roundsComplete + 1; i++) {
     std::cout << "Round " << i + 1 << ":" << std::endl;
     for (int j = 0; j < rounds[i].size(); j++) {
-      std::cout << "  Game " << j + 1 << ":  "
-                << std::left << std::setw(10) << rounds[i][j].Player1->getName() 
-                << " vs.   " 
-                << std::left << rounds[i][j].Player2->getName() 
-                << std::endl;
+      std::cout << "  Game " << j + 1 << ":  ";
+      if (rounds[i][j].winner) {
+        std::cout << GREEN << std::left << std::setw(10) << rounds[i][j].winner->getName() 
+                  << BASE << " vs.   " 
+                  << std::left << rounds[i][j].loser->getName() 
+                  << std::endl;
+      } else {
+        std::cout << std::left << std::setw(10) << rounds[i][j].Player1->getName() 
+                  << " vs.   " 
+                  << std::left << rounds[i][j].Player2->getName() 
+                  << std::endl;
+      }
     }
   }
 }
